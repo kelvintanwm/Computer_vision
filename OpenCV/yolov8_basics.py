f37758e4c6852beb61f0cc94b041d47ff222e185
KTWM <62528038+kelvintanwm@users.noreply.github.com>
2023-03-05 22:29:39 +0800

    Updets
    created yolov8_model.py

--- OpenCV/yolov8_basics.py
+++ OpenCV/yolov8_basics.py
@@ -5,7 +5,7 @@ import numpy
 # load a pretrained model 
 model = YOLO("yolov8n.pt","v8")
 
-image_path = r"C:\Users\Kelvin\Documents\GitHub\Coursera\OpenCV\Keene.jpg" #r"C:\Users\Kelvin\Documents\GitHub\Coursera\OpenCV\Streets.jpg"
+image_path = r"OpenCV\Keene.jpg" #r"C:\Users\Kelvin\Documents\GitHub\Coursera\OpenCV\Streets.jpg"
 
 # predict on an image
 # conf: Will be the minimum confidence of the model
---
+++ OpenCV/yolov8_model.py
@@ -0,0 +1,7 @@
+import numpy as np
+import cv2
+from ultralytics import YOLO
+import random
+
+# opening file 
+my_file = open()

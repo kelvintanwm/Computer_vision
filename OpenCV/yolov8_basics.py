e2c5ea17ec4b3dde734016ad837ac4027b1ce951
KTWM <62528038+kelvintanwm@users.noreply.github.com>
2023-05-31 23:22:36 +0800

    Update yolov8_basics.py

--- OpenCV/yolov8_basics.py
+++ OpenCV/yolov8_basics.py
@@ -5,14 +5,14 @@ import numpy
 # load a pretrained model 
 model = YOLO("yolov8n.pt","v8")
 
-image_path = r"OpenCV\Keene.jpg" #r"C:\Users\Kelvin\Documents\GitHub\Coursera\OpenCV\Streets.jpg"
+image_path = r"OpenCV\New_york.jpg" #r"C:\Users\Kelvin\Documents\GitHub\Coursera\OpenCV\Streets.jpg"
 
 # predict on an image
 # conf: Will be the minimum confidence of the model
 detection_output = model.predict(source= image_path , conf=0.4, save = True)
 
 # Display tensor array
-#print(detection_output)
+print(detection_output)
 
 # Print numpy array
 
